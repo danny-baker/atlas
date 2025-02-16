48c5e1b8e4b2102d5be38117d6e43a9cd09ee6bb
dan <daniel.robert.baker1@gmail.com>
2025-02-16 21:58:11 +1300

    testing baby

--- flask_app/dash_app/appv2.py
+++ flask_app/dash_app/appv2.py
@@ -10,4 +10,11 @@ print(sys.flags.debug)
 #successfully used brotli to compress master file hard from 97MB to 54MB. Nice.
 #going forward this means could periodically download titanium container and put into repo.
 
+#can't download folders directly from storage explorer (maybe on the proper app version??) but not
+# in browser version. Maybe make script that does this so it can be done easily in future?
 
+# script: pull titanium subfolders to new folder location called snapshot in /data/data_snapshot
+
+# 1. Get snapshot of processed data into repo
+# 2 Test debug mode can succesfully pull from different sources
+# 3 Begin the process of refactoring and rebuilding. First update to latest dash version etc. 
